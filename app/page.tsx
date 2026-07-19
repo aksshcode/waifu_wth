9f48e0286af9d2ec993e5add20e6bf096455af7b
Akssh <notnotsky26@gmail.com>
2026-07-19 08:16:27 +0530

    replace waifu swiper with placeholder layout using floating nav and card

--- app/page.tsx
+++ app/page.tsx
@@ -1,132 +1,39 @@
-'use client'
-import Image from "next/image"
-import { useEffect, useState } from "react"
-import { motion } from "motion/react"
-import { Spinner } from "@/components/ui/spinner"
-import { Button } from "@/components/ui/button"
-import { ArrowLeft, ArrowRight } from "lucide-react"
+import { Card } from "@/components/ui/card";
+import { FloatingNav } from "@/components/ui/floating-navbar"
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 
 export default function Home() {
-  type WaifuData = {
-    results: {
-      artist_name: string
-      artist_href: string
-      source_url: string
-      url: string
-      dimensions: { width: number; height: number }
-    }[]
-  }
-  const [fetchStatus, setFetchStatus] = useState<"success" | "loading" | "failed">("loading")
-  const [imgData, setImgData] = useState<WaifuData>()
-  const [nextImgData, setNextImgData] = useState<WaifuData>()
-  const imageData = imgData?.results[0]
-  const imageName = imageData?.artist_name ?? "loading"
-  async function getImgData(fromSwipe?: boolean) {
-    try {
-      const res = await fetch('https://nekos.best/api/v2/waifu')
-      const data = await res.json()
-      setImgData(data)
-      setSwipeDirection(null)
-      setFetchStatus("success")
-      if (fromSwipe) setSwipeKey(prev => prev + 1)
-    } catch {
-      setSwipeDirection(null)
-      setFetchStatus("failed")
-    }
-  }
-  async function getNextImgData(fromSwipe?: boolean) {
-    try {
-      const res = await fetch('https://nekos.best/api/v2/waifu')
-      const data = await res.json()
-      setNextImgData(data)
-      setImgData(prev => prev ?? data)
-      setSwipeDirection(null)
-      // setSwipeKey(prev => prev + 1)
-    } catch {
-      // fail silently
-    }
-  }
-  const [swipeDirection, setSwipeDirection] = useState<"left" | "right" | null>(null)
-  useEffect(() => {
-    getImgData()
-    getNextImgData()
-    function handleKey(e: KeyboardEvent) {
-      if (e.key === "ArrowLeft") swipeLeft()
-      if (e.key === "ArrowRight") swipeRight()
-    }
-    window.addEventListener("keydown", handleKey)
-    return () => window.removeEventListener("keydown", handleKey)
-  }, [])
-  const [swipeKey, setSwipeKey] = useState<number>(0)
-  function swipeLeft() {
-    if (!swipeDirection) {
-      setSwipeDirection("left")
-    }
-  }
-  function swipeRight() {
-    if (!swipeDirection) {
-      setSwipeDirection("right")
-    }
-  }
   return (
-    <div className="w-full h-screen flex-1 flex flex-col justify-center p-4 items-center ">
-      <div className="flex flex-col gap-4">
-        <motion.div
-          key={swipeKey}
-          initial={{ filter: "blur(2px)", scale: 0.25, opacity: 0 }}
-          animate={
-            swipeDirection === "right"
-              ? { filter: "blur(0px)", scale: 1, opacity: 0, x: 600 }
-              : swipeDirection === "left"
-                ? { filter: "blur(0px)", scale: 1, opacity: 0, x: -600 }
-                : { filter: "blur(0px)", scale: 1, opacity: 1, x: 0 }
-          }
-          transition={{ x: { duration: 0.15 }, opacity: { duration: 0.15 } }}
-          drag="x"
-          onDragEnd={(_, info) => {
-            if (info.offset.x > 100) setSwipeDirection("right")
-            else if (info.offset.x < -100) setSwipeDirection("left")
-            else setSwipeDirection(null)
-          }}
-          onAnimationComplete={() => {
-            if (swipeDirection) {
-              setImgData(nextImgData)
-              getNextImgData()
-              setSwipeKey(prev => prev + 1)
-              setSwipeDirection(null)
-            }
-          }}
-          dragSnapToOrigin={!swipeDirection}
-          className="relative rounded-xl overflow-clip w-[85vw] max-w-[450px] aspect-[9/16]">
-          {fetchStatus === "loading" ? <ImageSkeleton /> :
-            <Image
-              src={imageData?.url ?? 'https://picsum.photos/seed/picsum/200/300'}
-              alt="waifu image" fill className="object-cover" priority unoptimized />
-          }
-          <div className="inset-0 absolute bg-linear-to-t from-black to-transparent" />
-          <div className="absolute bottom-0 left-0 m-5">
-            {imageName}
-          </div>
-        </motion.div>
-        <motion.div
-          initial={{ filter: "blur(2px)", x: -10, opacity: 0 }}
-          animate={{ filter: "blur(0px)", x: 0, opacity: 1 }}
-          className="w-full flex-row flex"
-        >
-          <Button onClick={swipeLeft} className="m-1 w-auto flex-1"><ArrowLeft /></Button>
-          <Button onClick={swipeRight} className="m-1 w-auto flex-1"><ArrowRight /></Button>
-        </motion.div>
-      </div>
-    </div >
+    <div className="w-screen flex flex-col justify-start items-center gap-2 h-sccren flex-1 bg-gray-900">
+      <Header />
+      <Card className="p-4 w-1/2">haro there</Card>
+    </div>
   )
 }
 
-function ImageSkeleton() {
+const navItems = [
+  {
+    name: "Home",
+    link: "/",
+    icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "About",
+    link: "#about",
+    icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Contact",
+    link: "#contact",
+    icon: (
+      <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+    ),
+  },
+];
+function Header() {
   return (
-    <div
-      className="w-full h-full bg-[#0a0a0a] flex justify-center items-center rounded-xl"
-    >
-      <Spinner data-icon="inline-start" className="scale-200" />
+    <div className="relative mt-6 top-0 mb-24 w-full translate-z-0">
+      <FloatingNav className="absolute top-0 left-0 " navItems={navItems} />
     </div>
   )
 }
